5b5bb1de10359dd0255cbe846ebceb366e282285
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-09-16 11:12:57 +0100

    fixed issue

--- src/Components/Votes.jsx
+++ src/Components/Votes.jsx
@@ -1,51 +1,57 @@
-import { updateArticleVotes } from "./utils/api"
-import { useEffect, useState } from "react"
+import { updateArticleVotes } from "./utils/api";
+import { useEffect, useState } from "react";
+import { Icon } from '@iconify/react';
 
+export default function Votes({ currentVotes, setVotes, articleId }) {
+  const [localVoteAmount, setLocalVoteAmount] = useState(0);
+  const [isError, setIsError] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const [disliked, setDisliked] = useState(false);
 
-export default function Votes({currentVotes,setVotes,articleId}) {
-    const [localVoteAmount, setLocalVoteAmount] = useState(0)
-    const [isError, setIsError] = useState(false)
-    let apiVoteChange = 0
-
-    function handleClick(e,voteChange) {
-        if(localVoteAmount === 1 && voteChange === -1) {
-            apiVoteChange = -2
-        }
-        else if (localVoteAmount === -1 && voteChange === 1) {
-            apiVoteChange = 2
-        }
-
-        
-        setLocalVoteAmount(voteChange)
-        updateArticleVotes(articleId, { "inc_votes" : apiVoteChange})
-            .then((data) => {
-                setIsError(false)
-                return data
-
-            }).catch((err) => {
-                const errMessage = err.message
-                setIsError(errMessage)
-                console.log(errMessage)
-            })
-            }
-
-    useEffect(() => {
-        updateArticleVotes(articleId, { "inc_votes" : localVoteAmount})
-        .then((data) => {
-            console.log(data)
-        }).catch((err)=> {
-            console.log("ERROR", err)
+  function handleClick(e) {
+    const buttonId = (e.currentTarget.id)
+    if(buttonId === "up") {
+        setLiked((prev) => {
+            prev ? setLocalVoteAmount(0) : setLocalVoteAmount(1)
+            console.log(localVoteAmount)
+            return !prev
         })
-    }, [localVoteAmount])
+    }
+    else {
+        setDisliked((prev) => {
+            prev ? setLocalVoteAmount(0) : setLocalVoteAmount(-1)
+            return !prev
+        })
+    }
 
-    return !isError ?
-    ( 
-    <div className="votes-single-article">
-    <button onClick={(e) => {handleClick(e,1)}} disabled={localVoteAmount > 0}>Vote up</button>
-    <p>Votes: {currentVotes + localVoteAmount}</p>   
-    <button onClick={(e) => {handleClick(e,-1)}} disabled={localVoteAmount < 0}>Vote down</button>
-    </div>) :
-    <div className="vote-error">
-    <p>{isError} Please try again</p>
-    </div>
+    console.log(localVoteAmount)
+
+  }
+
+  useEffect(() => {
+    updateArticleVotes(articleId, { "inc_votes": (localVoteAmount)  })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("ERROR", err);
+      });
+  }, [localVoteAmount]);
+
+  
+  return (
+  <div className="vote-box">
+    <button id="up" onClick={(e) => {handleClick(e)}} >
+    <Icon icon="clarity:thumbs-up-solid" hFlip={true} color={liked ? "green" : "gray"}/>
+    </button>
+    <p>Votes : {currentVotes += localVoteAmount}</p>
+    <button id="down" onClick={(e) => {handleClick(e)}} >
+    <Icon icon="clarity:thumbs-up-solid" hFlip={true} vFlip={true} color={disliked ? "red" : "gray"}/>
+    </button>
+  </div>)
 }
+
+
+  
+    
+    
